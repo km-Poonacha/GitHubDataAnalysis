565c8ead2eac1cdafca1be23cf1d5620185935b3
P.K. Medappa <pmedappa@campus.uvt.nl>
2019-09-14 15:34:41 +0200

    Start building the app

--- GetCommits/GetCommit_GraphQL.py
+++ GetCommits/GetCommit_GraphQL.py
@@ -4,6 +4,17 @@ Created on Wed Sep 11 17:04:10 2019
 
 @author: pmedappa
 """
+import csv
+import sys
+if "C:\\Users\\pmedappa\\\Dropbox\\HEC\\Python\\CustomLib\\PooLIB" not in sys.path:
+    sys.path.append('C:\\Users\\pmedappa\\\Dropbox\\HEC\\Python\\CustomLib\\PooLIB')
+    print(sys.path)
+from poo_ghmodules import getGitHubapi
+from poo_ghmodules import ghpaginate
+from poo_ghmodules import ghparse_row
+
+import pandas as pd
+import numpy as np
 import requests
 
 def run_query(query): # A simple function to use requests.post to make the API call. Note the json= section.
@@ -60,6 +71,8 @@ query = """
             }
             """
 
+def get_name(repo_id):
+    
 headers = {"Authorization": "Bearer "+"dfb9844388015057b2bb8331c562068b04d9807f"}                        
 result = run_query(query)
 
