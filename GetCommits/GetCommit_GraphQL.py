6ae2b668c040b00a1f2c8b6e1d7c09904b639027
P.K. Medappa <pmedappa@campus.uvt.nl>
2019-09-17 15:56:27 +0200

    Main function running

--- GetCommits/GetCommit_GraphQL.py
+++ GetCommits/GetCommit_GraphQL.py
@@ -17,63 +17,72 @@ import pandas as pd
 import numpy as np
 import requests
 
-def run_query(query): # A simple function to use requests.post to make the API call. Note the json= section.
-    try:
-        request = requests.post('https://api.github.com/graphql', json={'query': query}, headers=headers)
-        return request.json()
-    except:
-        return '404'
-
-
-query = """
-            query{
-              rateLimit {
-                cost
-                remaining
-                resetAt
-              }
-              repository(name: "sickvim", owner: "jonathansick") {
-                ref(qualifiedName: "master") {
-                  target {
-                    ... on Commit {
-                      history(first:1 until: "2016-01-01T00:00:00Z"){
-                        totalCount
-                        pageInfo {
-                          endCursor
-                          hasNextPage
-                        }
-                            edges {
-                              node {
-                                comments{
-                                  totalCount
-                                }
-                                parents{
-                                  totalCount
-                                }
-                                changedFiles
-                                additions
-                                deletions
-                                messageHeadline
-                                oid
-                                message
-                                author {
-                                  name
-                                  email
-                                  date                        
-                                  }
+def run_query(name, owner): 
+    """ A simple function to use requests.post to make the API call. Note the json= section."""
+    headers = {"Authorization": "Bearer "+"dfb9844388015057b2bb8331c562068b04d9807f"}  
+    query = """
+                query{
+                  rateLimit {
+                    cost
+                    remaining
+                    resetAt
+                  }
+                  repository(name: """+name+""", owner: """+owner+""") {
+                    ref(qualifiedName: "master") {
+                      target {
+                        ... on Commit {
+                          history(first:100 until: "2016-01-01T00:00:00Z"){
+                            totalCount
+                            pageInfo {
+                              endCursor
+                              hasNextPage
+                            }
+                                edges {
+                                  node {
+                                    comments{
+                                      totalCount
+                                    }
+                                    parents{
+                                      totalCount
+                                    }
+                                    changedFiles
+                                    additions
+                                    deletions
+                                    messageHeadline
+                                    oid
+                                    message
+                                    author {
+                                      name
+                                      email
+                                      date                        
+                                      }
+                              }
+                            }
                           }
                         }
                       }
                     }
                   }
                 }
-              }
-            }
-            """
+                """
+    try:
+        request = requests.post('https://api.github.com/graphql', json={'query': query}, headers=headers)
+        return request.json()
+    except:
+        return '404'
 
 def get_name(repo_id):
     return
-headers = {"Authorization": "Bearer "+"dfb9844388015057b2bb8331c562068b04d9807f"}                        
-result = run_query(query)
 
-print(result)
+def main():
+    """Main function"""     
+#    name, owner = get_name(repo_id)  
+    name = None
+    owner = "jonathansick"
+    result = "0"
+    if name:                
+        result = run_query(name, owner)
+    
+    print(result)
+    
+main()
