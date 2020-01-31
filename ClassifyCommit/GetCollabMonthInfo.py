dd0edad2d2c86718a62514aadf03c81d5cdb3483
Km-Poonacha <km.poonacha@gmail.com>
2020-01-31 15:40:50 +0100

    Start code for consolidating collab info on a monthly basis

---
+++ ClassifyCommit/GetCollabMonthInfo.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 31 15:11:32 2020
+
+@author: pmedappa
+
+New code to find the collaborators for each project and date of their first contribution.
+Using the merge events sheet C:\Users\pmedappa\Dropbox\HEC\Project 5 - Roles and Coordination\Data\MergeEvents 
+"""
+
+
