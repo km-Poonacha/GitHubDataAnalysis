765b37d24818c7aa6596915adebb9e9507571494
P.K. Medappa <pmedappa@campus.uvt.nl>
2020-02-01 17:03:38 +0100

    Final rand java commit code

--- ClassifyCommit/RandCommits.py
+++ ClassifyCommit/RandCommits.py
@@ -3,34 +3,63 @@
 Created on Wed Nov 14 14:47:45 2018
 
 @author: kmpoo
+
+Generate a random sample of java commits. Remove all commits that do not have java files.
 """
 
 import pandas as pd
 
 import numpy as np
+import ast
 
-from datetime import datetime
+COMMIT_XL = 'C:/Data/092019 CommitInfo/JavaSampling/RANDJava_Commit.xlsx'
+COMMIT_UPDATE_XL = 'C:/Data/092019 CommitInfo/JavaSampling/upRANDJava_Commit_Sample.xlsx'
+COMMIT_SAMPLE_COMMON = 'C:/Data/092019 CommitInfo/JavaSampling/RANDJava_Commit_Sample_Com.xlsx'
+COMMIT_SAMPLE_SUM = 'C:/Data/092019 CommitInfo/JavaSampling/RANDJava_Commit_Sample_Sum.xlsx'
+COMMIT_SAMPLE_VED = 'C:/Data/092019 CommitInfo/JavaSampling/RANDJava_Commit_Sample_Ved.xlsx'
+def clean_files(x):
+    "Aggregae the probabilities calculated into a single construct"
+    text_file = ['java']    
+    if pd.notna(x['FILES']):
+        try:
+            files = ast.literal_eval(x['FILES'])
+        except: 
+            print("Remove ", x['SHA'])
+            return pd.Series([np.NaN,np.NaN,np.NaN,np.NaN,np.NaN], index=['REPO_ID','OWNER','NAME','SHA','FILES'])
+        if isinstance(files, (int)): return pd.Series([np.NaN,np.NaN,np.NaN,np.NaN,np.NaN], index=['REPO_ID','OWNER','NAME','SHA','FILES'])
+        for i in files:
+            l = i.split('.')
+            if len(l) > 1:
+                if l[1].lower() in text_file:
+                    return pd.Series(x,index=['REPO_ID','OWNER','NAME','SHA','FILES'])
 
-COMMIT_CSV = 'C:/Data/14_ContriCollab_2_off2.csv'
-COMMIT_SAMPLE_CSV = 'D:/Contributorinfo/14_Contricollab_Sample.csv'
+    return pd.Series([np.NaN,np.NaN,np.NaN,np.NaN,np.NaN], index=['REPO_ID','OWNER','NAME','SHA','FILES'])
+
+
 
 pd.options.display.max_rows = 10
 pd.options.display.float_format = '{:.1f}'.format
 
-commit_dataframe = pd.read_excel(COMMIT_CSV, sep=",",error_bad_lines=False,header = None,low_memory=False)
+commit_dataframe = pd.read_excel(COMMIT_XL, sep=",",error_bad_lines=False,header = 0)
 
-nrepo =  commit_dataframe.iloc[:,0].count()
-ncontri =  commit_dataframe.iloc[:,1].count() - nrepo
-ncommit = commit_dataframe.shape[0] - (ncontri + nrepo)
-print(" No of repos = ", nrepo ,"\n No of contri = ", ncontri, "\n No of commit = ", ncommit )
+print(" No of commit = ", commit_dataframe.shape[0] )
 
-commit_info_df = pd.DataFrame(commit_dataframe.loc[commit_dataframe[0].isnull() & commit_dataframe[1].isnull() ].iloc[:,2:14].copy()) 
-commit_info_df = commit_info_df.set_axis(['Sha', 'Date', 'Dsc', 'nComments', 'Committer', 'CommitterID', 'CommitDate','URL','Parents','Verified','Changes','Files'], axis='columns', inplace=False)
+commit_dataframe = commit_dataframe.rename(columns ={0: 'REPO_ID', 1: 'OWNER', 2: 'NAME', 3: 'SHA', 4: 'FILES'})
 
-commit_info_df = commit_info_df.assign(year = lambda x: x['Date'].str.slice(start=0, stop=4))
 
-print("Total commits extracted = ", commit_info_df.shape[0] )
-commit_info_df = commit_info_df.drop(commit_info_df[commit_info_df.year.isnull()].index)
-commit_info_df = commit_info_df.drop(commit_info_df[commit_info_df.year.astype(int) > 2015].index)
-print("Total commits before 2016 = ", commit_info_df.shape[0] )
-commit_info_df.sample(100).to_csv(COMMIT_SAMPLE_CSV)
+commit_dataframe2= commit_dataframe.apply(clean_files,  axis = 1)
+
+commit_dataframe2 = commit_dataframe2.dropna()
+
+print(" No of commit removed = ", commit_dataframe.shape[0] - commit_dataframe2.shape[0])
+
+print("Total commits = ", commit_dataframe2.shape[0] )
+commit_dataframe2 = commit_dataframe2.assign(url = lambda x: "http://github.com/"+x['NAME']+"/"+x['OWNER']+"/commit/"+x['SHA'])
+
+writer = pd.ExcelWriter(COMMIT_UPDATE_XL,options={'strings_to_urls': False})
+
+commit_dataframe2.to_excel(writer)
+
+commit_dataframe2.sample(100).to_excel(COMMIT_SAMPLE_COMMON)
+commit_dataframe2.sample(400).to_excel(COMMIT_SAMPLE_SUM)
+commit_dataframe2.sample(400).to_excel(COMMIT_SAMPLE_VED)
