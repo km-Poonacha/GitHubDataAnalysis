b6de3d7a94b6aa1d04ed4158567001404495c7d7
Km-Poonacha <km.poonacha@gmail.com>
2020-02-10 18:01:14 +0100

    After code run. updated basic info

--- ClassifyCommit/GetCollabMonthInfo_MonthWise.py
+++ ClassifyCommit/GetCollabMonthInfo_MonthWise.py
@@ -4,7 +4,8 @@ Created on Fri Jan 31 15:11:32 2020
 
 @author: pmedappa
 
-New code to find the collaborators for each project and date of their first contribution.
+New code to find the collaborators for each project and date of their first contribution.This code also groups collaborators
+,contributors and contributions monthwise.
 Using the merge events sheet C:/Users\pmedappa\Dropbox\HEC\Project 5 - Roles and Coordination\Data\MergeEvents 
 """
 
