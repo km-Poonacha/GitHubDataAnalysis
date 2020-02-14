ad78bbbdd4994b55cac53d4dd2468963828f22d1
Km-Poonacha <km.poonacha@gmail.com>
2020-02-14 11:46:25 +0100

    Run and test

---
+++ ClassifyCommit/AggClassifyCommit_nofor.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  2 10:30:42 2019
+
+@author: kmpoo
+
+Aggregate commit creativity at the level of months. Remove projects for which no commits were found. 
+"""
+
+import pandas as pd
+import numpy as np
+import ast
+
+REPOCOMMIT_LIST =[
+                   "C:/Data/092019 CommitInfo/ClassifiedRepoCommit/ClassifiedRepoCommit1_287_1.xlsx",
+                    "C:/Data/092019 CommitInfo/ClassifiedRepoCommit/ClassifiedRepoCommit288_500_1.xlsx"
+                    # "C:/Data/092019 CommitInfo/ClassifiedRepoCommit/ClassifiedRepoCommit501_1000_1.xlsx",
+                    # "C:/Data/092019 CommitInfo/ClassifiedRepoCommit/ClassifiedRepoCommit1001_1500_1.xlsx",
+                    # "C:/Data/092019 CommitInfo/ClassifiedRepoCommit/ClassifiedRepoCommit1501_2000_1.xlsx",
+                    # "C:/Data/092019 CommitInfo/ClassifiedRepoCommit/ClassifiedRepoCommit2002_2500_1.xlsx",
+                    # "C:/Data/092019 CommitInfo/ClassifiedRepoCommit/ClassifiedRepoCommit2501_3250_1.xlsx",
+                    # "C:/Data/092019 CommitInfo/ClassifiedRepoCommit/ClassifiedRepoCommit3251_4000_1.xlsx",
+                    # "C:/Data/092019 CommitInfo/ClassifiedRepoCommit/ClassifiedRepoCommit4001_5000_1.xlsx",
+                    # "C:/Data/092019 CommitInfo/ClassifiedRepoCommit/ClassifiedRepoCommit5001_5202_1.xlsx",
+                    # "C:/Data/092019 CommitInfo/ClassifiedRepoCommit/ClassifiedRepoCommit5203_6000_1.xlsx",
+                    # "C:/Data/092019 CommitInfo/ClassifiedRepoCommit/ClassifiedRepoCommit6001_6570_1.xlsx"
+                  ] 
+# COMMIT2_XLSX ="C:/Data/092019 CommitInfo/ClassifiedRepoCommit/ClassifiedRepoCommit1_287_1.xlsx"
+
+MC_XLSX = "C:/Data/092019 CommitInfo/ClassifiedRepoCommit/MC_RepoCommit1_287_1.xlsx"
+COMMITERS_XLSX = "C:/Data/092019 CommitInfo/Contributors_monthwise/Final_COL_MC_RepoCommit.xlsx"
+TEMP= "C:/Data/092019 CommitInfo/ClassifiedRepoCommit/temp.xlsx"
+def consolidate_prob(x, a1, a2,a3):
+    "Aggregae the probabilities calculated into a single construct"
+    text_file = ['txt','md','doc','docx','png','gif','jpg']
+    if pd.isna(x['PINDEX']) and pd.notna(x['OPEN_ISSUES']):
+        files = ast.literal_eval(x['OPEN_ISSUES'])
+        for i in files:
+            l = i.split('.')
+            if len(l) > 1:
+                if l[1].lower() not in text_file:
+                    return (x[a1]+x[a2]*2+x[a3]*3)/3
+
+    return np.NaN
+
+def consolidate_commits(df):
+    """ Consolidate commits monthly"""
+    if int(df.shape[0]) <1: return pd.DataFrame()
+    df.dropna(subset=['con_novelty'],inplace = True)
+    df2 = df.groupby('c_month')['con_novelty', 'con_usefulness','Noveltys2p1','Noveltys2p2','Noveltys2p3','Usefulnesss2p1','Usefulnesss2p2','Usefulnesss2p3'].mean()
+    df2= pd.concat([df2, df.groupby('c_month')['con_novelty'].count(),df.groupby('c_month')['SIZE'].nunique()], axis = 1)
+    
+    # Change index from c_months 
+    df2= df2.reset_index()
+    df2.columns = ['nc_month','m_novelty','m_usefulness','m_Noveltys2p1','m_Noveltys2p2','m_Noveltys2p3','m_Usefulnesss2p1','m_Usefulnesss2p2','m_Usefulnesss2p3','c_count','c_contributors']
+
+    return df2
+    
+def main():
+    pd.options.display.max_rows = 10
+    pd.options.display.float_format = '{:.3f}'.format
+
+    df_commiters= pd.read_excel(COMMITERS_XLSX, sep=",",error_bad_lines=False,header=0,  encoding = "Latin1")
+    writer = pd.ExcelWriter(MC_XLSX , engine='xlsxwriter')
+    for COMMIT_XLSX in REPOCOMMIT_LIST:
+        df_commit= pd.read_excel(COMMIT_XLSX, sep=",",error_bad_lines=False,header=0,  encoding = "Latin1")
+        df_commit.drop(df_commit[(df_commit.PINDEX.notna()) & (df_commit.PINDEX.shift(-1).notna())].index, inplace= True)
+        #clean_df(df_commit)
+        df_commit.drop(columns=["Unnamed: 0"], inplace = True)
+        
+        #get month information for each commit
+        df_commit = df_commit.assign( c_month =   pd.DatetimeIndex(pd.to_datetime(df_commit['OWNER'], infer_datetime_format=True,errors='coerce')).month)
+        df_commit = df_commit.assign( c_day =   pd.DatetimeIndex(pd.to_datetime(df_commit['OWNER'], infer_datetime_format=True,errors='coerce')).day)
+        #consolidate novelty and usefulness
+        df_commit['con_novelty'] = df_commit.apply(consolidate_prob, args =('Noveltys2p1','Noveltys2p2','Noveltys2p3'), axis = 1)
+        df_commit['con_usefulness'] = df_commit.apply(consolidate_prob, args =('Usefulnesss2p1','Usefulnesss2p2','Usefulnesss2p3'), axis = 1)
+        
+        #aggregate to monthly
+        
+        #get commits of a repo
+        repo_commits = pd.DataFrame()
+        write_commits = pd.DataFrame()
+        indx = df_commit.columns
+        indx = indx.append(pd.Index(['end_colab','end_cont','event_month','month','start_colab','start_cont','m_novelty','m_usefulness','m_Noveltys2p1','m_Noveltys2p2','m_Noveltys2p3','m_Usefulnesss2p1','m_Usefulnesss2p2','m_Usefulnesss2p3','c_count','c_contributors']))
+        
+    
+        repo_df = df_commit.dropna(subset=['PINDEX'])
+    
+        # df_commit= df_commit.assign(n_REPO_ID = lambda x : "" if pd.isna(x['PINDEX']) else x['REPO_ID'])
+        df_commit['n_REPO_ID'] = np.where(pd.isna(df_commit['PINDEX']), np.nan, df_commit['REPO_ID'])
+        
+        df_commit['n_REPO_ID'] = df_commit['n_REPO_ID'].fillna(method='ffill')
+        
+        df_commiters[0] = df_commiters[0].fillna(method='ffill')
+        
+        for i,row in repo_df.iterrows():
+            print(row['REPO_ID'])
+            repo_commits = df_commit[df_commit['n_REPO_ID']==row['REPO_ID']]
+            
+            repo_commits = repo_commits.iloc[1:]
+    
+            df_mcommit = consolidate_commits(repo_commits)
+            write_commits = write_commits.append(row, sort = False, ignore_index = True)
+            
+            
+            # get the monthwise commiters for the repo 
+            repo_commiters = df_commiters[df_commiters[0].astype(int).astype(str)==row['REPO_ID']]
+            
+            repo_commiters = repo_commiters[['end_colab','end_cont','event_month','month','start_colab','start_cont']]
+            
+            repo_commiters = repo_commiters.iloc[1:]  
+            if df_mcommit.shape[0] > 0:
+                repo_commiters = pd.concat([repo_commiters.set_index('month'),df_mcommit.set_index('nc_month')], axis=1).reset_index().rename(columns={'index':'month'}) 
+            else:
+                print("CHECK REPO FOR COMMITS ", row['REPO_ID'])
+               
+                    
+            write_commits = write_commits.append(repo_commiters, sort = False, ignore_index = True)                               
+    
+        write_commits = write_commits.reindex(indx, axis=1)
+        write_commits = write_commits.drop(axis=1,columns=['REPO_ID.1', 'yhat','opt_deg_sup_ind','opt_deg_sup_org','Unnamed: 104','UNKNOWN','c_month','c_day','con_novelty','con_usefulness','Noveltys2p1',	'Noveltys2p2',	'Noveltys2p3',	'Usefulnesss2p1',	'Usefulnesss2p2',	'Usefulnesss2p3'])
+        write_commits.to_excel(writer , sheet_name='Sheet1', index=False)
+        writer.save()
+        
+        # Save the full labeled data sample post processing in CSV
+   
+if __name__ == '__main__':
+  main()
+  
