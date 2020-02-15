687980d8b8aceb5c8fd6491b7fe0bde10171e6d1
P.K. Medappa <pmedappa@campus.uvt.nl>
2020-02-15 09:38:23 +0100

    Changes to excel append. Use helper function

--- ClassifyCommit/GetCollabMonthInfo_MonthWise.py
+++ ClassifyCommit/GetCollabMonthInfo_MonthWise.py
@@ -9,7 +9,7 @@ New code to find the collaborators for each project and date of their first cont
 Using the merge events sheet C:/Users\pmedappa\Dropbox\HEC\Project 5 - Roles and Coordination\Data\MergeEvents 
 """
 
-
+import openpyxl
 import pandas as pd
 import numpy as np
 import ast
@@ -47,14 +47,83 @@ def monthwise(contri):
 
     return df2
 
+def append_df_to_excel(filename, df, sheet_name='Sheet1', startrow=None,
+                       truncate_sheet=False, 
+                       **to_excel_kwargs):
+    """
+    Append a DataFrame [df] to existing Excel file [filename]
+    into [sheet_name] Sheet.
+    If [filename] doesn't exist, then this function will create it.
+
+    Parameters:
+      filename : File path or existing ExcelWriter
+                 (Example: '/path/to/file.xlsx')
+      df : dataframe to save to workbook
+      sheet_name : Name of sheet which will contain DataFrame.
+                   (default: 'Sheet1')
+      startrow : upper left cell row to dump data frame.
+                 Per default (startrow=None) calculate the last row
+                 in the existing DF and write to the next row...
+      truncate_sheet : truncate (remove and recreate) [sheet_name]
+                       before writing DataFrame to Excel file
+      to_excel_kwargs : arguments which will be passed to `DataFrame.to_excel()`
+                        [can be dictionary]
+
+    Returns: None
+    """
+    from openpyxl import load_workbook
+
+    import pandas as pd
+
+    # ignore [engine] parameter if it was passed
+    if 'engine' in to_excel_kwargs:
+        to_excel_kwargs.pop('engine')
+
+    writer = pd.ExcelWriter(filename, engine='openpyxl')
+
+    # Python 2.x: define [FileNotFoundError] exception if it doesn't exist 
+
+
+
+    try:
+        # try to open an existing workbook
+        writer.book = load_workbook(filename)
+
+        # get the last row in the existing Excel sheet
+        # if it was not specified explicitly
+        if startrow is None and sheet_name in writer.book.sheetnames:
+            startrow = writer.book[sheet_name].max_row
+
+        # truncate sheet
+        if truncate_sheet and sheet_name in writer.book.sheetnames:
+            # index of [sheet_name] sheet
+            idx = writer.book.sheetnames.index(sheet_name)
+            # remove [sheet_name]
+            writer.book.remove(writer.book.worksheets[idx])
+            # create an empty sheet [sheet_name] using old index
+            writer.book.create_sheet(sheet_name, idx)
+
+        # copy existing sheets
+        writer.sheets = {ws.title:ws for ws in writer.book.worksheets}
+    except FileNotFoundError:
+        # file does not exist yet, we will create it
+        pass
+
+    if startrow is None:
+        startrow = 0
+
+    # write out the new sheet
+    df.to_excel(writer, sheet_name, startrow=startrow, **to_excel_kwargs)
+
+    # save the workbook
+    writer.save()
+    
 def main():
     """Get data from the training sample CSV and perform various cleaning and data preprocessing"""
 
     pd.options.display.max_rows = 10
     pd.options.display.float_format = '{:.3f}'.format
-    writer = pd.ExcelWriter(COL_MC_XLSX , engine='xlsxwriter')
-    writer2 = pd.ExcelWriter(NEW_XLSX , engine='xlsxwriter')
-    writer3 = pd.ExcelWriter(DT_ERROR_LOG  , engine='xlsxwriter')
+
     for iterate in range(1,8):
         print("******** File ", iterate," ********")
 
@@ -64,6 +133,8 @@ def main():
         write_xl = pd.DataFrame()
         month_colab = pd.DataFrame()
         e_log = pd.DataFrame()
+        print("NUmber of repos : ", repo_df.shape[0])
+
         for i, repo in repo_df.iterrows():
             # print(repo)
             write_xl = write_xl.append(repo, sort = False, ignore_index = True)
@@ -92,7 +163,7 @@ def main():
             write_events = events.drop(events[events[3] == 'PullRequestEvent'].index)
     
             collaborators = write_events[2].unique()
-            print("Repo : ",repo[0]," contributors ",len(contributors)," collaborators ",len(collaborators))
+            # print("Repo : ",repo[0]," contributors ",len(contributors)," collaborators ",len(collaborators))
             
             # find first occurance of collborators
             for c in collaborators:
@@ -120,13 +191,12 @@ def main():
             month_data.rename(columns={"index": "month"}, inplace = True)
 
             month_colab = pd.concat([month_colab,month_data],sort = False)
-            
-        write_xl.to_excel(writer , sheet_name='Sheet1', index=False)
-        writer.save()
-        month_colab.to_excel(writer2 , sheet_name='Sheet1', index=False)
-        writer2.save()
-        e_log.to_excel(writer3 , sheet_name='Sheet1', index=False)
-        writer3.save()
+        
+  
+        append_df_to_excel(COL_MC_XLSX, write_xl,index=False)  
+        append_df_to_excel(NEW_XLSX, month_colab, index=False)
+        append_df_to_excel(DT_ERROR_LOG, e_log, index=False)  
+
 if __name__ == '__main__':
   main()
   
