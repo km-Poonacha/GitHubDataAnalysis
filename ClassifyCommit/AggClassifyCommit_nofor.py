4f985609315c53e7a7bd5b092dad84c16e888a33
P.K. Medappa <pmedappa@campus.uvt.nl>
2020-02-15 10:23:37 +0100

    Added excel append function

--- ClassifyCommit/AggClassifyCommit_nofor.py
+++ ClassifyCommit/AggClassifyCommit_nofor.py
@@ -25,8 +25,6 @@ REPOCOMMIT_LIST =[
                     # "C:/Data/092019 CommitInfo/ClassifiedRepoCommit/ClassifiedRepoCommit5203_6000_1.xlsx",
                     # "C:/Data/092019 CommitInfo/ClassifiedRepoCommit/ClassifiedRepoCommit6001_6570_1.xlsx"
                   ] 
-# COMMIT2_XLSX ="C:/Data/092019 CommitInfo/ClassifiedRepoCommit/ClassifiedRepoCommit1_287_1.xlsx"
-
 MC_XLSX = "C:/Data/092019 CommitInfo/ClassifiedRepoCommit/MC_RepoCommit1_287_1.xlsx"
 COMMITERS_XLSX = "C:/Data/092019 CommitInfo/Contributors_monthwise/Final_COL_MC_RepoCommit.xlsx"
 TEMP= "C:/Data/092019 CommitInfo/ClassifiedRepoCommit/temp.xlsx"
@@ -55,14 +53,87 @@ def consolidate_commits(df):
     df2.columns = ['nc_month','m_novelty','m_usefulness','m_Noveltys2p1','m_Noveltys2p2','m_Noveltys2p3','m_Usefulnesss2p1','m_Usefulnesss2p2','m_Usefulnesss2p3','c_count','c_contributors']
 
     return df2
+
+def append_df_to_excel(filename, df, sheet_name='Sheet1', startrow=None,
+                       truncate_sheet=False, 
+                       **to_excel_kwargs):
+    """
+    Append a DataFrame [df] to existing Excel file [filename]
+    into [sheet_name] Sheet.
+    If [filename] doesn't exist, then this function will create it.
+
+    Parameters:
+      filename : File path or existing ExcelWriter
+                 (Example: '/path/to/file.xlsx')
+      df : dataframe to save to workbook
+      sheet_name : Name of sheet which will contain DataFrame.
+                   (default: 'Sheet1')
+      startrow : upper left cell row to dump data frame.
+                 Per default (startrow=None) calculate the last row
+                 in the existing DF and write to the next row...
+      truncate_sheet : truncate (remove and recreate) [sheet_name]
+                       before writing DataFrame to Excel file
+      to_excel_kwargs : arguments which will be passed to `DataFrame.to_excel()`
+                        [can be dictionary]
+
+    Returns: None
+    """
+    from openpyxl import load_workbook
+
+    import pandas as pd
+
+    # ignore [engine] parameter if it was passed
+    if 'engine' in to_excel_kwargs:
+        to_excel_kwargs.pop('engine')
+
+    writer = pd.ExcelWriter(filename, engine='openpyxl')
+
+    # Python 2.x: define [FileNotFoundError] exception if it doesn't exist 
+
+
+
+    try:
+        # try to open an existing workbook
+        writer.book = load_workbook(filename)
+
+        # get the last row in the existing Excel sheet
+        # if it was not specified explicitly
+        if startrow is None and sheet_name in writer.book.sheetnames:
+            startrow = writer.book[sheet_name].max_row
+
+        # truncate sheet
+        if truncate_sheet and sheet_name in writer.book.sheetnames:
+            # index of [sheet_name] sheet
+            idx = writer.book.sheetnames.index(sheet_name)
+            # remove [sheet_name]
+            writer.book.remove(writer.book.worksheets[idx])
+            # create an empty sheet [sheet_name] using old index
+            writer.book.create_sheet(sheet_name, idx)
+
+        # copy existing sheets
+        writer.sheets = {ws.title:ws for ws in writer.book.worksheets}
+    except FileNotFoundError:
+        # file does not exist yet, we will create it
+        pass
+
+    if startrow is None:
+        startrow = 0
+
+    # write out the new sheet
+    df.to_excel(writer, sheet_name, startrow=startrow, **to_excel_kwargs)
+
+    # save the workbook
+    writer.save() 
+    
     
 def main():
     pd.options.display.max_rows = 10
     pd.options.display.float_format = '{:.3f}'.format
 
     df_commiters= pd.read_excel(COMMITERS_XLSX, sep=",",error_bad_lines=False,header=0,  encoding = "Latin1")
-    writer = pd.ExcelWriter(MC_XLSX , engine='xlsxwriter')
+
     for COMMIT_XLSX in REPOCOMMIT_LIST:
+        print(COMMIT_XLSX)
         df_commit= pd.read_excel(COMMIT_XLSX, sep=",",error_bad_lines=False,header=0,  encoding = "Latin1")
         df_commit.drop(df_commit[(df_commit.PINDEX.notna()) & (df_commit.PINDEX.shift(-1).notna())].index, inplace= True)
         #clean_df(df_commit)
@@ -94,7 +165,6 @@ def main():
         df_commiters[0] = df_commiters[0].fillna(method='ffill')
         
         for i,row in repo_df.iterrows():
-            print(row['REPO_ID'])
             repo_commits = df_commit[df_commit['n_REPO_ID']==row['REPO_ID']]
             
             repo_commits = repo_commits.iloc[1:]
@@ -119,8 +189,9 @@ def main():
     
         write_commits = write_commits.reindex(indx, axis=1)
         write_commits = write_commits.drop(axis=1,columns=['REPO_ID.1', 'yhat','opt_deg_sup_ind','opt_deg_sup_org','Unnamed: 104','UNKNOWN','c_month','c_day','con_novelty','con_usefulness','Noveltys2p1',	'Noveltys2p2',	'Noveltys2p3',	'Usefulnesss2p1',	'Usefulnesss2p2',	'Usefulnesss2p3'])
-        write_commits.to_excel(writer , sheet_name='Sheet1', index=False)
-        writer.save()
+
+        append_df_to_excel(MC_XLSX, write_commits,index=False)
+        print("number of rows : ", write_commits.shape[0])
         
         # Save the full labeled data sample post processing in CSV
    
